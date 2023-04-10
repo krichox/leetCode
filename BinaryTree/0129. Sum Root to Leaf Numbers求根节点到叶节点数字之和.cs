a46ffdafafdb0f64738e40b28fc16e3d40cdba90
krichox <krichox@gmail.com>
2023-04-10 15:26:49 +0800

    4.9

---
+++ BinaryTree/0129. Sum Root to Leaf Numbers求根节点到叶节点数字之和.cs
@@ -0,0 +1,27 @@
+﻿namespace BinaryTree
+{
+    /*https://leetcode.cn/problems/sum-root-to-leaf-numbers/description/*/
+    public class Sum_Root_to_Leaf_Numbers求根节点到叶节点数字之和 {
+        public int SumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
+    
+        int dfs(TreeNode root, int preSum)
+        {
+            if(root == null)
+            {
+                return 0;
+            }
+
+            var sum = preSum * 10 + root.Val;
+            if(root.Left == null && root.Right == null)
+            {
+                return sum;
+            }
+            else
+            {
+                return dfs(root.Left, sum) + dfs(root.Right, sum);
+            }
+        }
+    }
+}
